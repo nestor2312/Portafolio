ca70c0b89ba7d2a27439e2fb10de6527dd1e230c
nestor2312 <91697971+nestor2312@users.noreply.github.com>
2024-04-19 11:45:55 -0500

    vf

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('.nav');
-const closeBtn = document.querySelector('#close');
+let inicio = document.getElementById('inicio');
+let closeBtn = document.querySelector('#close');
+let Sobre_mi = document.getElementById('Sobre_mi');
+let Skills = document.getElementById('Skills');
+let Formación = document.getElementById('Formación');
+let Proyectos = document.getElementById('Proyectos');
 
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('visible');
 });
 
 closeBtn.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+Sobre_mi.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+Skills.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+Formación.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+Proyectos.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+inicio.addEventListener('click', () => {
     nav.classList.remove('visible');
 });
